c49d060b2da5cd4480c8d34183c5204241cd16f7
松本雄揮 <yukiji.0307@gmail.com>
2021-08-01 11:06:36 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2>MY APP!!!</h2>
 
         <p className={styles.description}>
           Get started by editing{' '}
